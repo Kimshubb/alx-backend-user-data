e54e0ecbf440a317518846e5098ca0ddb3fa1017
Kimshubb <issakimanifmk@gmail.com>
2024-09-30 06:15:08 +0000

    add user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -39,8 +39,9 @@ class DB:
         Return: Newly created user object
         """
         user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
+        session = self._session
+        session.add(user)
+        session.commit()
         return user
 
     def find_user_by(self, **kwargs) -> User:
